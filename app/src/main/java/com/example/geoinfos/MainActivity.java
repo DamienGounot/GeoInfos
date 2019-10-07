b33263f87ead9a340b50757d63d1090f76b08626
Damien Gounot <gounot@et.esiea.fr>
2019-10-07 10:47:48 +0200

    Handling Menu item click
    + creation of fragment (localisation feature)
    + creation of fragment (picture from camera feature)
    + creation of fragment (api feature)
    
    + navigation between fragment using navigation menu

--- app/src/main/java/com/example/geoinfos/MainActivity.java
+++ app/src/main/java/com/example/geoinfos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.geoinfos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,36 @@ private DrawerLayout drawer;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_open);
         drawer.addDrawerListener(toggle);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+            navigationView.setCheckedItem(R.id.search);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+            case R.id.localize:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LocalizeFragment()).commit();
+                break;
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+                break;
+            case R.id.camera:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PictureFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     public void onBackPressed(){
         if(drawer.isDrawerOpen(GravityCompat.START)){
