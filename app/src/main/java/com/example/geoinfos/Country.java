555c678907014142289c1faf82089a350e61e80b
Damien Gounot <gounot@et.esiea.fr>
2019-11-03 18:44:01 +0100

    Updating from Textview to Listview
    + Implementation of Listview
    - removing Textview (not Material Design)

--- app/src/main/java/com/example/geoinfos/Country.java
+++ app/src/main/java/com/example/geoinfos/Country.java
@@ -1,5 +1,9 @@
 package com.example.geoinfos;
 
+import android.graphics.Movie;
+
+import java.util.List;
+
 public class Country {
 
     private String name;
@@ -31,5 +35,14 @@ public class Country {
 
     public String getFlag() {
         return flag;
+    }
+
+    public Country(String name, String nativeName, String capital, String region, int population, String flag) {
+        this.name = name;
+        this.nativeName = nativeName;
+        this.capital = capital;
+        this.region = region;
+        this.population = population;
+        this.flag = flag;
     }
 }
--- app/src/main/java/com/example/geoinfos/SearchFragment.java
+++ app/src/main/java/com/example/geoinfos/SearchFragment.java
@@ -1,17 +1,21 @@
 package com.example.geoinfos;
 
+import android.graphics.Movie;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import android.widget.ArrayAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,12 +25,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class SearchFragment extends Fragment {
 
     private TextView textViewResult;
+    private ListView listViewResult;
+    private ArrayList<String> ARcountries;
+    private  ArrayAdapter<String> arrayAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
      View view = inflater.inflate(R.layout.fragment_search,container,false);
            textViewResult = view.findViewById(R.id.text_view_result);
+            listViewResult =  view.findViewById(R.id.list_view_result);
+
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://restcountries.eu/rest/v2/")
@@ -46,23 +55,23 @@ public class SearchFragment extends Fragment {
                         }
 
                         List<Country> countries = response.body();
-                        for (Country country : countries){
-                            String content = "";
-                            content += "Name : " + country.getName() + "\n";
-                            content += "Native Name : " + country.getNativeName() + "\n";
-                            content += "Capital : " + country.getCapital() + "\n";
-                            content += "Region : " + country.getRegion() + "\n";
-                            content += "Population : " + country.getPopulation() + "\n";
-                            content += "Flag : " + country.getFlag() + "\n\n";
 
-                            textViewResult.append(content);
-                        }
+
+                ARcountries = new ArrayList<>();
+                for (Country country : countries){
+                ARcountries.add("\nCountry : " + country.getName() + "\n" + "Capital : " + country.getCapital() + "\n" + "Region : " + country.getRegion() + "\n" + "Population : " + country.getPopulation() + "\n" + "Flag : " + country.getFlag() + "\n");
+                }
+                arrayAdapter = new ArrayAdapter<>(getActivity().getBaseContext(), android.R.layout.simple_list_item_1,ARcountries);
+
+
+                       listViewResult.setAdapter(arrayAdapter);
             }
 
             @Override
             public void onFailure(Call<List<Country>> call, Throwable t) {
                     textViewResult.setText(t.getMessage());
             }
+
         });
         return view;
     }
