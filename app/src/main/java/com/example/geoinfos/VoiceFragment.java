674cb2261bf4f4e87d9400d7110932fe7f33e293
Damien Gounot <gounot@et.esiea.fr>
2019-12-06 10:38:48 +0100

    Text to Speach
    + adding TTS methods
    + TTS when returning the view
    + TTS when button clic

--- app/src/main/java/com/example/geoinfos/VoiceFragment.java
+++ app/src/main/java/com/example/geoinfos/VoiceFragment.java
@@ -1,19 +1,86 @@
 package com.example.geoinfos;
 
+import java.util.Locale;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class VoiceFragment extends Fragment {
+public class VoiceFragment extends Fragment implements
+        TextToSpeech.OnInitListener {
+    /** Called when the activity is first created. */
 
-    @Nullable
+    private TextToSpeech tts;
+    private Button btnSpeak;
+    private TextView txtText;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_voice, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_voice, container, false);
+
+        super.onCreate(savedInstanceState);
+
+        tts = new TextToSpeech( getActivity(), VoiceFragment.this );
+        btnSpeak = view.findViewById(R.id.getVoice);
+        txtText = view.findViewById(R.id.voice_result);
+
+        // button on click event
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Failed!");
+        }
+
+    }
+
+    private void speakOut() {
+
+        String text = txtText.getText().toString();
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
